ef5ee761909d00c25158c747c6acefd24780833e
go-max07 <johnmetry17@gmail.com>
2021-06-14 10:47:58 +0300

    add name change to create github live page

--- script/script.js
+++ script/script.js
@@ -1,65 +1,41 @@
-    let weekDays = [
+function getAkanName() {
+    let varYear = document.getElementById("varYear").value;
+    let varMonth = Number(document.getElementById("varMonth").value);
+    let varDate = Number(document.getElementById("varDate").value);
+    let genders = document.getElementsByName("gender");
 
-        "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
-        "Saturday"
-
-    ];
-
-    //arrays for akan names for the males
-
-    let akanMale = [
-
-        "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"
-
-    ];
-
-    //arrays for akan names for the females
-
-    let akanFemale = [
-
-        "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"
-
-    ];
-
-    function getAkanName() {
-        let varYear = document.getElementById("varYear").value;
-        let varMonth = Number(document.getElementById("varMonth").value);
-        let varDate = Number(document.getElementById("varDate").value);
-        let genders = document.getElementsByName("gender");
-
-        // function to get gender
-        function getGender() {
-            for (let gender of genders) {
-                if (gender.checked) {
-                    return gender.value;
-                }
+    // function to get gender
+    function getGender() {
+        for (let gender of genders) {
+            if (gender.checked) {
+                return gender.value;
             }
         }
+    }
 
-        let varGender = getGender();
-        console.log(varGender);
+    let varGender = getGender();
+    console.log(varGender);
 
-        // validation function for the month 
-        function valMonth() {
-            if (varMonth < 1 || varMonth > 12) {
-                return false;
-            } else {
+    // validation function for the month to make sure user enters a month between 1 and 12. Input type nummber in html ignores min and max length properties
+    function valMonth() {
+        if (varMonth < 1 || varMonth > 12) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+
+
+    //validation function for days of the month.
+    function valDay() {
+        //leap year
+        if (varMonth === 2 && Number(varYear % 4 === 0)) {
+            if (varDate >= 1 && varDate <= 29) {
                 return true;
-            }
-        }
-
-
-
-
-        //validation function for days of the month.
-        function valDay() {
-            //leap year
-            if (varMonth === 2 && Number(varYear % 4 === 0)) {
-                if (varDate >= 1 && varDate <= 29) {
-                    return true;
-                } else {
-                    return false;
-                }
+            } else {
+                return false
             }
         }
         //non leap year
@@ -82,7 +58,7 @@
         //validation of the 31 days
         else if (varMonth === 1 || varMonth === 3 || varMonth === 5 || varMonth === 7 || varMonth === 8 || varMonth === 10 || varMonth === 12) {
             if (varDate >= 1 && varDate <= 31) {
-                return true;
+                return true
             } else {
                 return false;
             }
@@ -101,6 +77,18 @@
     let dayNumber = Math.floor((((Number(varYear.slice(0, 2)) / 4) - 2 * Number(varYear.slice(0, 2)) - 1) +
         ((5 * Number(varYear.slice(2, 4)) / 4)) + ((26 * (varMonth + 1) / 10)) + varDate) % 7);
 
+    //arrays for days of the week
+    let weekDays = [
+        "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+    ];
+    //arrays for akan names for the males
+    let akanMale = [
+        "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"
+    ];
+    //arrays for akan names for the females
+    let akanFemale = [
+        "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"
+    ];
 
     //selecting items on the arrays
     let index;
@@ -130,3 +118,4 @@
     } else {
         alert("Date error. Make sure you entered a\r valid number of days for that month");
     }
+}
